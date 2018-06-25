4e004b822ce57fe5caa2d9803ea9f2ee8d476fcd
rakesh kumar <rakesh635@gmail.com>
2018-06-25 14:29:44 +0530

    added new files

---
+++ config.php
@@ -0,0 +1,37 @@
+
+
+    <?php
+
+    /* Database credentials. Assuming you are running MySQL
+
+    server with default setting (user 'root' with no password) */
+
+    define('DB_SERVER', 'mysql-db.cinbwplpl4rh.ap-south-1.rds.amazonaws.com');
+
+    define('DB_USERNAME', 'root');
+
+    define('DB_PASSWORD', 'password');
+
+    define('DB_NAME', 'testdb');
+
+     
+
+    /* Attempt to connect to MySQL database */
+
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+     
+
+    // Check connection
+
+    if($link === false){
+
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+
+    }
+
+    ?>
+    
+    
+
+
